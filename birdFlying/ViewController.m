2964c09dd13d55d2760ede9b7881c70d7d96a9ca
Apple <Apple@Apples-MacBook-Air.local>
2015-12-27 10:53:26 +0700

    first commit

--- birdFlying/ViewController.m
+++ birdFlying/ViewController.m
@@ -12,12 +12,73 @@
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    UIImageView *backgroundforest;
+    UIImageView *bird;
+}
+
+- (void) initproject{
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self initproject];
+    [self forestCreating];
+    [self birdCreating];
+    [self flyupdown];
 }
+- (void) forestCreating{
+
+    backgroundforest = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"jungle.jpg"]];
+    backgroundforest.frame = self.view.bounds;
+    [self.view addSubview:backgroundforest];
+    
+    
+}
+- (void) birdCreating{
+    bird = [[UIImageView alloc]initWithFrame:CGRectMake(0,0,110,68)];
+    
+    NSMutableArray *image = [[NSMutableArray alloc]initWithCapacity:6];
+    
+    
+    for (int i=0; i<6; i++){
+        NSString *name = [NSString stringWithFormat:@"bird%i.png",i];
+        [image addObject:[UIImage imageNamed:name]];
+    }
+    
+    bird.animationImages = image;
+    bird.animationDuration=1;
+    bird.animationRepeatCount=0;
+    
+    [self.view addSubview:bird];
+    [bird startAnimating];
+    
+  
+            
+    
+}
+- (void) flyupdown{
+    
+ bird.transform = CGAffineTransformIdentity;
+   
+    [UIView animateWithDuration:4 animations:^{
+        bird.center = CGPointMake(self.view.bounds.size.width,self.view.bounds.size.height);
+        
+    }completion:^(BOOL finished){
+        bird.transform = CGAffineTransformConcat(CGAffineTransformMakeScale(-1,1),
+                                                 CGAffineTransformMakeRotation(M_PI_4));
+        
+        [UIView animateWithDuration:6 animations:^{
+            bird.center = CGPointMake(0, 0);
+        }completion:^(BOOL finished){
+            [self flyupdown];
+        }
+         ];
+    }];
+     
+     }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
